669117847d31b1cf333e2b25aa9425d19fde5868
jpcorreia99 <jpcorreia99@gmail.com>
2020-05-17 00:21:05 +0100

    implementar a* inicial

--- src/main.pl
+++ src/main.pl
@@ -78,9 +78,6 @@ depth_first_exclui_operadores( Caminho, Paragem, End,ListaOperadores,Solucao,Dis
 
 
 %ex 4
-
-
-
 identifica_maior_carreiras(Comeco, Destino,Solucao,Distancia,ListaOrdenadaPorNumeroDeCarreiras):-
     depthfirst( [], Comeco, Destino,SolucaoInvertida,Distancia),
     reverse(SolucaoInvertida,Solucao),
@@ -104,7 +101,7 @@ converte_tuplo_para_n_carreiras((GID,_),(GID,NCarreiras)):-
 
 
 
-goal(182).
+%goal(182).
 
 resolve_gulosa(Partida, Destino,Caminho/Custo) :-
 	estima(Partida,Destino ,Estimativa),
@@ -146,3 +143,60 @@ obtem_melhor_g([Caminho1/Custo1/Est1,_/Custo2/Est2|Caminhos], MelhorCaminho) :-
 	
 obtem_melhor_g([_|Caminhos], MelhorCaminho) :- 
 	obtem_melhor_g(Caminhos, MelhorCaminho).
+
+
+
+
+
+/*
+adjacencia(183,791,87.63541293336934,1).
+adjacencia(791,595,87.63541293336934,1).
+adjacencia(595,182,421.9863463431075,1).
+adjacencia(182,499,2003.3340491291042,1).
+
+paragem(183,-103678.36,-96590.26,'Bom','Fechado dos Lados','Yes','Vimeca',286,'Rua Aquilino Ribeiro','Carnaxide e Queijas').
+paragem(791,-103705.46,-96673.6,'Bom','Fechado dos Lados','Yes','Vimeca',286,'Rua Aquilino Ribeiro','Carnaxide e Queijas').
+paragem(595,-103725.69,-95975.2,'Bom','Fechado dos Lados','Yes','Vimeca',286,'Rua Aquilino Ribeiro','Carnaxide e Queijas').
+paragem(182,-103746.76,-96396.66,'Bom','Fechado dos Lados','Yes','Vimeca',286,'Rua Aquilino Ribeiro','Carnaxide e Queijas').
+paragem(499,-103758.44,-94393.36,'Bom','Fechado dos Lados','Yes','Vimeca',286,'Rua Aquilino Ribeiro','Carnaxide e Queijas').*/
+
+
+%goal(499).
+
+
+resolve_aestrela(Nodo, Destino,Caminho/Custo) :-
+	estima(Nodo, Destino,Estima),
+	aestrela([[Nodo]/0/Estima],Destino ,InvCaminho/Custo/_),
+	reverse(InvCaminho, Caminho).
+
+aestrela(Caminhos,Destino,SolucaoCaminho) :-
+	obtem_melhor(Caminhos, MelhorCaminho),
+	seleciona(MelhorCaminho, Caminhos, OutrosCaminhos),
+	expande_aestrela(MelhorCaminho,Destino,ExpCaminhos),
+	append(OutrosCaminhos, ExpCaminhos, NovoCaminhos),
+	aestrela(NovoCaminhos,Destino ,SolucaoCaminho).	
+
+aestrela(Caminhos,Destino ,Caminho) :-
+        obtem_melhor(Caminhos, Caminho),
+        Caminho = [Node|_]/_/_,
+        Node == Destino.
+
+expande_aestrela(Caminho,Destino ,ExpCaminhos) :-
+	findall(NovoCaminho, move_aestrela(Caminho,Destino,NovoCaminho), ExpCaminhos).
+
+move_aestrela([Nodo|Caminho]/Custo/_,Destino ,[ProxNodo,Nodo|Caminho]/NovoCusto/Est) :-
+	adjacencia(Nodo, ProxNodo, PassoCusto,_),\+ member(ProxNodo, Caminho),
+	NovoCusto is Custo + PassoCusto,
+	estima(ProxNodo,Destino ,Est).	
+	
+
+obtem_melhor([Caminho], Caminho) :- !.
+
+obtem_melhor([Caminho1/Custo1/Est1,_/Custo2/Est2|Caminhos], MelhorCaminho) :-
+	Custo1 + Est1 =< Custo2 + Est2, !, % custo 1 é a soma dos caminhos até aí
+	obtem_melhor([Caminho1/Custo1/Est1|Caminhos], MelhorCaminho).
+	
+obtem_melhor([_|Caminhos], MelhorCaminho) :- 
+	obtem_melhor(Caminhos, MelhorCaminho).
+
+
